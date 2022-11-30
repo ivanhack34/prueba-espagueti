d13d4566bce05493f12066eff6bc3b4c94888b96
jcarlosdf <josedelgado1987@gmail.com>
2022-11-29 23:18:58 -0500

    commit init

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 
+const db = require('./utils/database')
+
 const port = 9000
 
-const app = express
+const app = express()
+
+db.authenticate()
+.then(log=>console.log("Database has been authenticated!"))
+.catch(err=>console.error(err.message))
+
+db.sync()
+.then(log=>console.log("Synchronization successfully!"))
+.catch(err=>console.error(err.message))
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Ok!'})
 }) 
 
-app.listen(() => {
+app.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
